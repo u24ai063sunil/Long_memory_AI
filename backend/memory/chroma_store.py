4740f0624f829b10c4d2ff9a67d291a8b3bf180d
u24ai063sunil <u24ai063@aid.svnit.ac.in>
2026-02-13 15:42:58 +0530

    removed unnecessary parts

--- backend/memory/chroma_store.py
+++ backend/memory/chroma_store.py
@@ -1,19 +1,3 @@
-# import chromadb
-# from chromadb.utils import embedding_functions
-
-# client = chromadb.Client(
-#     chromadb.Settings(persist_directory="./chroma_memory")
-# )
-
-# embedding_function = embedding_functions.SentenceTransformerEmbeddingFunction(
-#     # model_name="all-MiniLM-L6-v2"
-#     model_name="paraphrase-MiniLM-L3-v2"
-# )
-
-# memory_collection = client.get_or_create_collection(
-#     name="long_term_memory",
-#     embedding_function=embedding_function
-# )
 import chromadb
 from chromadb.utils import embedding_functions
 import os
